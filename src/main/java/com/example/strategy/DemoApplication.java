e63f9ef72810590674af8eb124f47c3e7b8d47b5
anderson_lin <anderson_lin@auer.com.tw>
2026-02-03 11:00:57 +0800

    refactor: remove unused imports

--- src/main/java/com/example/strategy/DemoApplication.java
+++ src/main/java/com/example/strategy/DemoApplication.java
@@ -1,12 +1,7 @@
 package com.example.strategy;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.scheduling.annotation.Scheduled;
-
-import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
 public class DemoApplication {
